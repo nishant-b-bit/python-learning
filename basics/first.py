48a67f975bd644b142e36ec7009ebc024bfd3e5c
nishant-b-bit <nishantb9822@gmail.com>
2025-11-16 21:26:14 +0545

    Add files via upload

---
+++ basics/first.py
@@ -0,0 +1,11 @@
+name="nishant"
+age=20
+price=25.99
+old=True #true is boolean value
+a=None #same here none is also none value
+
+print(type(name))
+print(type(age))
+print(type(price))
+print(type(old))
+print(type(a))
