55b0b4342adfeb9b60782d22de49d2e994a890d3
Nishant Budha <nishantb9822@gmail.com>
2025-11-17 21:16:14 +0545

    Add files via upload

---
+++ basics/string_Q.py
@@ -0,0 +1,3 @@
+#write a program to input user's first name and print it's length
+name=input("enter your name: ")
+print("length of your name is: ",len(name))
---
+++ basics/string_Q2..py
@@ -0,0 +1,3 @@
+#wap to find the occurrence of $ in a string
+currency="exchange rate of $ in nepali currency and exchange rate of $ in indian currency"
+print(currency.count("$"))
