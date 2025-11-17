f8db854669a16286ea8407292f7c68c6faa2fb98
nishant-b-bit <nishantb9822@gmail.com>
2025-11-17 06:13:04 +0545

    Add files via upload

--- basics/first_Q2.py
+++ basics/first_Q2.py
@@ -1,5 +1,4 @@
 #write a program to input side of square and print its area
  
-length=float(input("enter length: "))#here i am taking float value because i thougt leght and breadth might be in point as well but you can take the value in integer
-breadth=float(input("enter breadth: "))
-print("area of square is: ",length*breadth)
+side=float(input("enter side: "))#here i am taking float value because i thougt side might be in point as well but you can take the value in integer
+print("area of square is: ",side*side)
