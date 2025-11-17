53b632859eecffda102eed8cfbc18684fcb41e1e
nishant-b-bit <nishantb9822@gmail.com>
2025-11-17 08:05:22 +0545

    Add files via upload

---
+++ basics/first_Q3.py
@@ -0,0 +1,5 @@
+#program to input 2 floating point numbers and print their average
+
+num1=float(input("enter first number: "))
+num2=float(input("enter second number: "))
+print("average of both number is",(num1+num2)/2)
---
+++ basics/first_Q4.py
@@ -0,0 +1,15 @@
+#write a program to input 2 int numbers a and b print true if a is greater than or equal to b. if not print false
+
+"""
+a=int(input("enter first number: "))
+b=int(input("enter second number: "))
+if a>=b:
+    print("true")
+else:
+    print("flase")
+"""
+#upper one is also write but we can also use relational operator
+
+a=int(input("enter first number: "))
+b=int(input("enter second number: "))
+print(a>=b)#as relational operator checks the condition and gives answer in true and false
