f4fa40aacf8103ad18b33661f4c6e7f14f2126a2
Nishant Budha <nishantb9822@gmail.com>
2025-11-19 05:15:15 +0545

    Add files via upload

---
+++ basics/list_1.py
@@ -0,0 +1,12 @@
+# Indexing in list
+
+name=["nishant",23,34.5,"budha"]
+print(name[2])#just like in string indexing is possible in listing
+# VVIM in string we were only allowed to acess the value by indexing but were couldn't change the value but here in listing it is possible
+name[2]="gita"
+print(name)
+
+#slicing in list
+
+marks=[45,56,43,56,44]
+print(marks[1:])#slicing is same as in string
---
+++ basics/list_2.py
@@ -0,0 +1,24 @@
+#list methods
+
+#append
+list=[7,6,3,4]
+list.append(5)#why i write like this i explained in video of phone saved in programming self
+print(list)
+
+#sort
+list.sort()
+print(list)
+
+#sort(reverse=True)
+list.sort(reverse=True)
+print(list)
+
+#reverse
+list.reverse()
+print(list)
+
+#insert
+list.insert(0,8)
+print(list)
+#remove
+#pop
