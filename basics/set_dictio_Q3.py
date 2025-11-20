6e93602eef90938d97fd08064635771da3e30f9a
Nishant Budha <nishantb9822@gmail.com>
2025-11-20 22:17:57 +0545

    Add files via upload

---
+++ basics/set_dictio_Q3.py
@@ -0,0 +1,5 @@
+#figure out the way to store 9 and 9.0 as seprate values in the set
+values={"9.0",9}
+print(values)
+
+#or next method is to store like values={("float",9.0),9} but values={"9.0",9 will still give the same value}
