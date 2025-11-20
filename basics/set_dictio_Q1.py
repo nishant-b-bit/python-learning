a35be097ce427a2be7414454ef317a556d5e2900
Nishant Budha <nishantb9822@gmail.com>
2025-11-20 21:31:47 +0545

    Add files via upload

---
+++ basics/set_dictio_Q1.py
@@ -0,0 +1,8 @@
+# store following word meaning in a pyhthon dictionary:
+#table:"a piece of furniture","list of facts or figures"
+#cat:"a small animal"
+information={
+    "table":["a piece of furniture","list of facts and figures"],#here we wrote table and it's both value in list because it has 2 values but we can also store it in tuple
+    "cat":"a small animal"
+}
+print(information)
