8c7129857806ee9e6971a1b59f4525af991359c1
Nishant Budha <nishantb9822@gmail.com>
2025-11-22 08:39:49 +0545

    Add files via upload

---
+++ basics/while_loop_Q2.py
@@ -0,0 +1,6 @@
+# print numbers from 100 to 1
+
+num=100
+while num>=1:
+    print(num)
+    num-=1
---
+++ basics/while_loop_Q3.py
@@ -0,0 +1,17 @@
+# print the multiplication table of a number n
+# table of 5
+
+
+x=1
+table=5*x
+while table<=50:
+    print(table)
+    x+=1
+    table=5*x
+
+# next method for talbe of 3
+
+x=1
+while x<=10:
+    print(3*x)
+    x+=1
