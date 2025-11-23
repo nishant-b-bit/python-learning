e26c7bfd688c69da5cfd9f55a75385633d369882
Nishant Budha <nishantb9822@gmail.com>
2025-11-23 06:16:14 +0545

    Add files via upload

---
+++ basics/for_loop1.py
@@ -0,0 +1,8 @@
+num=[1,2,3]
+for pr in num: # it is not compulsory to assign pr, you can write anything you want
+    print(pr)
+
+str="Nishant"
+for char in str:
+    print(char) # each and every characters will be printed
+    
