35f362bb5d28c68a30c604d65bcb00a7f6aeeaae
Nishant Budha <nishantb9822@gmail.com>
2025-11-24 09:56:20 +0545

    Add files via upload

---
+++ basics/function_Q2.py
@@ -0,0 +1,9 @@
+# write a function to print the lenght of a list(list is the parameter)
+
+def lenght(lists):
+    print(len(lists))
+    return lists
+num=[1,2,45,2,42]
+cloth=["hat","shirt"]
+x=lenght(num)
+y=lenght(cloth)
